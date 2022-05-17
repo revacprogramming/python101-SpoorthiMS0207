da6cfb421e3c2444881a1d1705b4b885a51d79e7
Spoorthi M S <97089639+SpoorthiMS0207@users.noreply.github.com>
2022-05-17 15:01:09 +0000

    print largest and smallest of entered numbers

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -2,22 +2,24 @@
 
 largest = None
 smallest = None
+
 while True:
     num = input("Enter a number: ")
-
-    if num == "done":
-        break
-
-    try:
-      num = int(num)
-      if largest is None or largest < num:
-          largest = num
-      if smallest is None or smallest > num:
-          smallest = num
-
-    except:
-      print('Invalid input')
-      continue
-
-print("Maximum is", largest)
-print("Minimum is", smallest)
+    if num == 'done':
+        break;
+    try :
+        n = int(num)
+    except :
+       print ("Invalid input")
+    
+    
+    
+    if largest is None :
+        largest = n
+    elif n >largest:
+        largest = n
+    if smallest is None :
+        smallest = n
+    elif n <smallest:
+        smallest = n
+    
