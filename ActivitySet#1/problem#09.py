6a5bdc137b5cef5ee3e15938fb63cad7164bb11e
Spoorthi M S <97089639+SpoorthiMS0207@users.noreply.github.com>
2022-05-29 15:22:26 +0000

    Lists

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,12 +1,14 @@
 # Lists
-fname=input ("Enter the file name")
-fh=open(fname)
-lst=list()
-for line in fh :
-  line=line.strip()
-for words in word:
-  word=word strip()
-    
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    line=line.strip()
+    words=line.split()
+for word in words : 
+    if word not in lst:
+        list.append(word)
+print(sorted(lst))
 
 
 
