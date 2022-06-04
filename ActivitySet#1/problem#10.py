2522663b383373fd0c8835b7d471e71517c8600c
Spoorthi M S <97089639+SpoorthiMS0207@users.noreply.github.com>
2022-06-04 10:15:55 +0000

    Continuation of dictionary

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -4,3 +4,13 @@ filename = "dataset/mbox-short.txt"
 name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 text = open(name)
+max=dict()
+for line in text :
+  line.rstrip()
+  if not line.startswith ("from"):
+
+
+
+
+
+
