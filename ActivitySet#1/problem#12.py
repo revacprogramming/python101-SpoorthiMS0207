a9409e60e7ecc0c73f54e9fcf5ef7e6894f855d9
Spoorthi M S <97089639+SpoorthiMS0207@users.noreply.github.com>
2022-07-03 10:59:13 +0000

    done

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,14 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+import re
+
+hand = open('a.txt')
+all_lines = hand.read() 
+all_str_nums_as_one_line = re.findall('[0-9]+',all_lines)
+hand.close()
+tot = 0
+for str_num in all_str_nums_as_one_line:
+    tot += int(str_num)
+
+print('Overall sum is:',tot) 
+
