85e09d712572006cf4ffe7c9b0310f8c8d684426
Spoorthi M S <97089639+SpoorthiMS0207@users.noreply.github.com>
2022-07-08 16:39:58 +0000

    activity set 2

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,12 +1,9 @@
 
 
-def add(a, b):
-    return  # ...
+def add(a,b):
+  sum=a+b
+  return sum
+num1=int(input("input the first number: "))
+num2=int(input("input the second number:"))
+print("The sum is",add(num1,num2))
 
-
-def main():
-    a = input  # ...
-    b = input  # ...
-
-    c = add(a, b)
-    print  # ...
